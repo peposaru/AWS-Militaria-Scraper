0f1477cc1324c0b860eccd3b26eabbe5d9175b89
Keenan Nilson <keenanfujimoto@gmail.com>
2024-11-21 15:03:41 +0900

    Moved HISCOLL to first site for testing. And Increased target match to 1000.

--- AWS_MILITARIA_SCRAPER_JSON.py
+++ AWS_MILITARIA_SCRAPER_JSON.py
@@ -285,7 +285,7 @@ def main():
     productsProcessed = 0
     
     # Set how many in a row you want to match
-    targetMatch = 300
+    targetMatch = 1000
 
     # Opening the JSON file containing website specific selectors
     with open(selectorJson,'r') as userFile:
